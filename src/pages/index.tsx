c29893526e23181d3fa615f6dc83633a2a974286
James Dunseith <jdunseith@gimbalabs.io>
2023-02-19 12:01:48 -0500

    Move /components to /src/components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 
-import Navbar from "@/components/Navbar";
-import Hero from "@/components/Hero";
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
 
 export default function Home() {
 
