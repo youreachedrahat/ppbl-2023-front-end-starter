a144f6aebc3b7d182071721c9d1fe9075df536fc
jamesdunseith <jdunseith@gimbalabs.io>
2023-02-22 16:15:41 -0500

    Fix merge conflicts

--- src/pages/modules/101/index.tsx
+++ src/pages/modules/101/index.tsx
@@ -1,46 +1,36 @@
-import Sidebar from "@/src/components/ui/Text/Sidebar";
 import Status101 from "@/src/components/course-modules/101/Status101";
 import React from "react";
-
 import Head from "next/head";
-import Lesson1011 from "./lesson-1011";
-import Lesson1012 from "./lesson-1012";
-import Lesson1013 from "./lesson-1013";
-import Lesson1014 from "./lesson-1014";
-import SLTs101 from "./101-SLTs";
+
+import { default as Example } from "./example.mdx"
+import { Box, Heading, Text } from "@chakra-ui/react";
+import Link from "next/link";
+import Sidebar from "@/src/components/ui/Text/Sidebar";
 
 const items = [
   {
-    name: "Student Learning Targets",
-    content: <SLTs101 />,
-  },
-  {
-    name: "Lesson 1",
-    content: <><Status101 /> <br/> <Lesson1011 /></>,
-  },
-  {
-    name: "Lesson 2",
-    content: <><Status101 /> <br/> <Lesson1012 /></>,
-  },
-  {
-    name: "Lesson 3",
-    content: <><Status101 /> <br/> <Lesson1013 /></>,
-  },
-  {
-    name: "Lesson 4",
-    content: <><Status101 /> <br/> <Lesson1014 /></>,
+    name: "Check",
+    content:
+    <>
+      <Box>
+        <Example />
+        <Heading>Module 101: Compile your first...</Heading>
+        <Text>This is a .tsx file</Text>
+        <Text><Link href="./101/example">Click here</Link> to see an .mdx file.</Text>
+      </Box>
+    </>,
   },
 ];
 
 const Module101 = () => {
   return (
     <>
-    <Head>
-      <title>PPBL Module 101</title>
-    </Head>
-    <div>
-      <Sidebar items={items} />
-    </div>
+      <Head>
+        <title>PPBL Module 101</title>
+      </Head>
+      <div>
+        <Sidebar items={items} />
+      </div>
     </>
   );
 };
