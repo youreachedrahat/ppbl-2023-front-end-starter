d3ff8da64dfa4900647a7c089e8546c818d5d637
nelson-ksh <nelsonksh2021@gmail.com>
2023-03-07 04:36:27 +0530

    some routing fixes

--- src/pages/modules/101/index.tsx
+++ src/pages/modules/101/index.tsx
@@ -1,25 +1,20 @@
 import Sidebar from "@/src/components/ui/Text/Sidebar";
-import Status100 from "@/src/components/course-modules/100/Status100";
 import React from "react";
-import { default as Example } from "@/src/components/course-modules/101/Example.mdx";
 import Head from "next/head";
 
-import { items } from "@/src/data/modules/100";
+import { items } from "@/src/data/modules/101";
 
 import { Grid, GridItem } from "@chakra-ui/react";
 
 const Module101 = () => {
   return (
     <>
-      <Head>
-        <title>PPBL Module 101</title>
-      </Head>
-      <Grid templateRows="repeat(10, 1fr)" templateColumns="repeat(6, 1fr)">
-        <Sidebar items={items} modulePath="/modules/101" selected={0} />
-        <GridItem colSpan={5}>
-          <Example />
-        </GridItem>
-      </Grid>
+    <Head>
+      <title>PPBL Module 100</title>
+    </Head>
+    <div>
+      <Sidebar items={items} modulePath="/modules/101" selected={0} />
+    </div>
     </>
   );
 };
