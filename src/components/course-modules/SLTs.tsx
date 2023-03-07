00b896939a7eae83356ae5899ce18c7c7d416e03
jamesdunseith <jdunseith@gimbalabs.io>
2023-03-07 10:58:43 -0500

    Add release dates to SLT View

--- src/components/course-modules/SLTs.tsx
+++ src/components/course-modules/SLTs.tsx
@@ -1,4 +1,4 @@
-import { Container, Heading, List, ListItem, Text } from "@chakra-ui/react";
+import { Container, Divider, Heading, List, ListItem, Text } from "@chakra-ui/react";
 import React from "react";
 import slts from "@/src/data/slts.json";
 
@@ -13,15 +13,17 @@ interface SLTsItemsProps {
 }
 
 const SLTsItems = ({ moduleTitle, moduleNumber }: SLTsItemsProps) => {
-  const currentModule = slts.modules.filter((module) => module.number == moduleNumber)
-  const items: SLT[] = currentModule[0].slts
+  const currentModule = slts.modules.filter((module) => module.number == moduleNumber)[0]
+  const items: SLT[] = currentModule.slts
 
   return (
     <Container maxWidth="80%" marginTop="2em">
-      <Text>{moduleTitle}</Text>
-      <Heading size="2xl" color="theme.blue">Student Learning Targets</Heading>
+      <Heading py="5">PPBL Module {currentModule.number}: {currentModule.title}</Heading>
+      <Text pb="5">Release Date: {currentModule.releaseDate} | Updated: {currentModule.updatedDate}</Text>
+      <Divider />
+      <Heading size="2xl" color="theme.blue" py="5">Student Learning Targets</Heading>
 
-        <List mt="2em" lineHeight="2">
+        <List lineHeight="2">
             {items.map((item, index) => {
                 const [id, SLT] = [item.id, item.slt];
                 return (
