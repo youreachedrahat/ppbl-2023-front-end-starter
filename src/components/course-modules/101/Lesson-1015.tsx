f638e48c998b1e11e303f57f4e906aec9140020d
jamesdunseith <jdunseith@gimbalabs.io>
2023-03-13 15:52:45 -0400

    Draft Lesson 101.5

--- src/components/course-modules/101/Lesson-1015.tsx
+++ src/components/course-modules/101/Lesson-1015.tsx
@@ -16,16 +16,30 @@ import AssignmentComponent from "@/src/components/lms/Lesson/AssignmentComponent
 import SuccessComponent from "@/src/components/lms/Lesson/SuccessComponent";
 import LessonLayout from "@/src/components/lms/Lesson/LessonLayout";
 import VideoComponent from "@/src/components/lms/Lesson/VideoComponent";
-import Docs1015 from "@/src/components/course-modules/101/Docs1015.mdx"
+import Docs1015 from "@/src/components/course-modules/101/Docs1015.mdx";
 
 export default function Lesson1015() {
   return (
     <LessonLayout moduleNumber={101} sltId="101.5" slug="1015">
       <Grid mx="auto" fontSize="lg" fontWeight="bold" lineHeight="200%" templateColumns="repeat(2, 1fr)" gap={6}>
         <GridItem w="90%" mx="auto">
-          <Text py="3">Write details about the lesson here</Text>
-          <Text py="3">Write details about the lesson here</Text>
-          <Text>Share some links, reference the video</Text>
+          <Text py="3">
+            <CLink href="https://aiken-lang.org/" target="_blank">
+              Aiken
+            </CLink>{" "}
+            is a{" "}
+            <CLink href="https://aiken-lang.org/#faq" target="_blank">
+              new programming language and toolkit for developing smart contracts
+            </CLink>{" "}
+            on Cardano.
+          </Text>
+          <Text py="3">
+            The Aiken language was written in Rust, but Aiken is not Rust. When we use Aiken, we will use some Rust
+            tooling.
+          </Text>
+          <Text py="3">
+            The <CLink href="https://aiken-lang.org/getting-started" target="_blank">Aiken documentation</CLink> is very helpful. We recommend taking a look!
+          </Text>
         </GridItem>
         <GridItem>
           <VideoComponent videoId="">VIDEO DESCRIPTION</VideoComponent>
