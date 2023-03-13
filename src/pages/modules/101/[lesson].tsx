5a3c86589ef6cfa8ee71c04fbfc1e7502ed1afc6
jamesdunseith <jdunseith@gimbalabs.io>
2023-03-13 08:03:55 -0400

    Add placeholder to Module 101

--- src/pages/modules/101/[lesson].tsx
+++ src/pages/modules/101/[lesson].tsx
@@ -1,21 +1,22 @@
 import SLTs101 from "@/src/components/course-modules/101/101-SLTs";
-import Lesson1011 from "@/src/components/course-modules/101/Lesson-1011";
-import Lesson1012 from "@/src/components/course-modules/101/Lesson-1012";
-import Lesson1013 from "@/src/components/course-modules/101/Lesson-1013";
-import Lesson1014 from "@/src/components/course-modules/101/Lesson-1014";
+// import Lesson1011 from "@/src/components/course-modules/101/Lesson-1011";
+// import Lesson1012 from "@/src/components/course-modules/101/Lesson-1012";
+// import Lesson1013 from "@/src/components/course-modules/101/Lesson-1013";
+// import Lesson1014 from "@/src/components/course-modules/101/Lesson-1014";
 import { default as Example } from "@/src/components/course-modules/101/Example.mdx";
-import Status101 from "@/src/components/course-modules/101/Status101";
+// import Status101 from "@/src/components/course-modules/101/Status101";
 import ModuleLessons from "@/src/components/course-modules/Lesson";
 import slt from "@/src/data/slts-english.json"
 
 import ComingSoon from "@/src/components/course-modules/ComingSoon";
+import Status100 from "@/src/components/course-modules/100/Status100";
 
 
 const Module101Lessons = () => {
 
   const moduleSelected = slt.modules.find((m) => m.number === 101);
 
-  const status = <Status101 />
+  const status = <Status100 />
 
   // Sidebar items are generated from module.lessons i.e. from the JSON file
   // Here we simply set the contents by matching the slug and key
@@ -31,7 +32,7 @@ const Module101Lessons = () => {
   return (
     <ModuleLessons items={moduleSelected?.lessons ?? []} modulePath="/modules/101" selected={0} lessons={lessons} status={status} />
   )
-          
+
 };
 
 export default Module101Lessons;
