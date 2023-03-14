e19b4cdc644f986ac84daa69ebaa26ef0f36e418
Sebastian Pabon <ps.joan3@gmail.com>
2023-03-13 22:57:18 -0500

    end of test modification .json

--- src/pages/modules/100/index.tsx
+++ src/pages/modules/100/index.tsx
@@ -2,7 +2,7 @@ import Sidebar from "@/src/components/ui/Text/Sidebar";
 import React from "react";
 import Head from "next/head";
 
-import slt from "@/src/data/slts-japanese.json"
+import slt from "@/src/data/slts-english.json"
 
 const Module100 = () => {
 
