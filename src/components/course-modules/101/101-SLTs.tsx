c31536f2960ad29f64558b9c0079b21fdfbd839d
James Dunseith <jdunseith@gimbalabs.io>
2023-03-16 16:37:27 -0400

    Add button, copy edits

--- src/components/course-modules/101/101-SLTs.tsx
+++ src/components/course-modules/101/101-SLTs.tsx
@@ -1,9 +1,9 @@
 import SLTsItems from "@/src/components/lms/Lesson/SLTs";
-import { Container, Divider, Heading, Box } from "@chakra-ui/react";
+import { Container, Divider, Box, Button, Spacer, Flex } from "@chakra-ui/react";
+import Link from "next/link";
 import React from "react";
 
 import Introduction from "@/src/components/course-modules/101/Introduction.mdx";
-
 
 const SLTs101 = () => {
   return (
@@ -13,6 +13,12 @@ const SLTs101 = () => {
       <Box py="5">
         <Introduction />
       </Box>
+      <Flex direction="row">
+        <Spacer />
+      <Link href="/modules/101/1011">
+        <Button my="1em">Take me to Lesson 101.1: Compiling Plutus Scripts</Button>
+      </Link>
+      </Flex>
     </Container>
   );
 };
