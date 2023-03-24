98a6c721208a631292109a5630f148326f59b364
jamesdunseith <jdunseith@gimbalabs.io>
2023-03-24 09:14:37 -0400

    Draft Mastery Assignment 102.2

--- src/components/course-modules/102/Assignment1021.tsx
+++ src/components/course-modules/102/Assignment1021.tsx
@@ -2,7 +2,13 @@ import AssignmentComponent from "@/src/components/lms/Lesson/AssignmentComponent
 
 import module from "./module102.json";
 import Assignment1021 from "@/src/components/course-modules/102/Assignment1021.mdx";
+import GetHelp from "../../lms/Course/GetHelp";
 
 export default function Assignment1021Page() {
-  return <Assignment1021 />;
+  return (
+    <>
+      <Assignment1021 />
+      <GetHelp />
+    </>
+  );
 }
--- src/components/course-modules/102/Assignment1022.tsx
+++ src/components/course-modules/102/Assignment1022.tsx
@@ -2,7 +2,13 @@ import AssignmentComponent from "@/src/components/lms/Lesson/AssignmentComponent
 
 import module from "./module102.json";
 import Assignment1022 from "@/src/components/course-modules/102/Assignment1022.mdx";
+import GetHelp from "../../lms/Course/GetHelp";
 
 export default function Assignment1022Page() {
-  return <Assignment1022 />
+  return (
+    <>
+      <Assignment1022/>
+      <GetHelp />
+    </>
+  );
 }
