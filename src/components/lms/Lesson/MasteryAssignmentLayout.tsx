774653affbd3d5af17cc64f2b652eb0713c30df8
jamesdunseith <jdunseith@gimbalabs.io>
2023-03-27 13:54:59 -0400

    Update live coding calendar. Cherry pick this commit after merging module-102-planning

--- src/components/lms/Lesson/MasteryAssignmentLayout.tsx
+++ src/components/lms/Lesson/MasteryAssignmentLayout.tsx
@@ -24,7 +24,7 @@ const MasteryAssignmentLayout: React.FC<Props> = ({ children, nextButtonHref, ne
         {nextButtonHref && (
           <Flex direction="row">
             <Spacer />
-            <Link href={`/modules/${nextButtonHref}/slts`}>
+            <Link href={`/modules/${nextButtonHref}`}>
               <Button my="1em" colorScheme="green">{nextButtonTitle}</Button>
             </Link>
             <Spacer />
