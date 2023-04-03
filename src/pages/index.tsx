41c9a6af867187b09066f9383e2d5746c0d054f3
jamesdunseith <jdunseith@gimbalabs.io>
2023-04-03 08:38:14 -0400

    Add simple language links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 import GetHelp from "@/src/components/lms/Course/GetHelp";
 import Hero from "@/src/components/ui/Text/Hero";
 import { Box, Center, Divider, Link as CLink } from "@chakra-ui/react";
-import SocialLinks from "../components/lms/Course/SocialLinks";
+import LanguageList from "@/src/components/lms/Course/LanguageList";
 
 export default function Home() {
   return (
@@ -14,7 +14,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Hero />
-      <Divider w="70%" mx="auto" />
+      <LanguageList />
+      <Divider w="70%" mx="auto" pb="10" />
       <Box w={["100%", "70%"]} mx="auto" my="10">
         <GetHelp />
       </Box>
